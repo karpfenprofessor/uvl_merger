6d28503e251e1f49ec6310ffc8f57f861e5f6948
karpfenprofessor <manuel.moertl@outlook.com>
2024-05-11 22:08:31 +0200

    refined

--- app/src/main/java/fish/merge/ModelMerger.java
+++ app/src/main/java/fish/merge/ModelMerger.java
@@ -19,20 +19,36 @@ public class ModelMerger {
     protected final static Logger logger = LogManager.getLogger(ModelMerger.class);
 
     public static MergedModel mergeModels(BaseModel baseModel1, BaseModel baseModel2) {
-        MergedModel mergedBaseModel = new MergedModel(false, 0);
+        MergedModel baseMergedModel = new MergedModel(false, 0);
         HashMap<String, IntVar> variablesMap = new HashMap<>();
 
         contextualizeConstraints(baseModel1, "region", baseModel1.getRegionModel());
         contextualizeConstraints(baseModel2, "region", baseModel2.getRegionModel());
 
+        mergeVariables(baseModel1, baseModel2, baseMergedModel, variablesMap);
+
+        printAllVariables(baseMergedModel);
+        printAllConstraints(baseMergedModel);
+
+        // Transfer constraints from both models, ensuring references to unified
+        // variables
+        // addConstraintsToModel(model1, mergedModel, variablesMap);
+        // addConstraintsToModel(model2, mergedModel, variablesMap);
+
+        return baseMergedModel;
+    }
+
+    private static void mergeVariables(BaseModel baseModel1, BaseModel baseModel2, BaseModel baseMergedModel,
+            HashMap<String, IntVar> variablesMap) {
+        
+        logger.debug("Start merge Variables of models " + baseModel1.printRegion() + " and " + baseModel2.printRegion());
         Model model1 = baseModel1.getModel();
         Model model2 = baseModel2.getModel();
-        Model mergedModel = mergedBaseModel.getModel();
-
+        Model mergedModel = baseMergedModel.getModel();
         for (IntVar var : model1.retrieveIntVars(true)) {
-            if(baseModel1.getVariableNames().contains(var.getName())) {
+            if (baseModel1.getVariableNames().contains(var.getName())) {
                 IntVar mergedVar = mergedModel.intVar(var.getName(), var.getLB(), var.getUB());
-                //logger.info("m1 var new: " + mergedVar.toString());
+                // logger.info("m1 var new: " + mergedVar.toString());
                 variablesMap.put(var.getName(), mergedVar);
             }
         }
@@ -49,54 +65,45 @@ public class ModelMerger {
                 IntVar mergedVar = mergedModel.intVar(var.getName(), lowerBound, upperBound);
                 mergedModel.unassociates(existingVar);
                 // Update the map
-                //logger.info("m2 var dupl: " + mergedVar.toString());
+                // logger.info("m2 var dupl: " + mergedVar.toString());
                 variablesMap.put(var.getName(), mergedVar);
                 // Remove the previous variable definition from the model to avoid confusion
             } else if (baseModel2.getVariableNames().contains(var.getName())) {
                 // Create new variable if it doesn't exist
                 IntVar mergedVar = mergedModel.intVar(var.getName(), var.getLB(), var.getUB());
-                //logger.info("m2 var new: " + mergedVar.toString());
+                // logger.info("m2 var new: " + mergedVar.toString());
                 variablesMap.put(var.getName(), mergedVar);
             }
         }
-
-        printAllVariables(mergedBaseModel);
-        printAllConstraints(mergedBaseModel);
-
-        // Transfer constraints from both models, ensuring references to unified
-        // variables
-        // addConstraintsToModel(model1, mergedModel, variablesMap);
-        // addConstraintsToModel(model2, mergedModel, variablesMap);
-
-        return mergedBaseModel;
+        logger.debug("Finished merge Variables of models " + baseModel1.printRegion() + " and " + baseModel2.printRegion());
     }
 
     private static void printAllVariables(BaseModel m) {
-        logger.debug("-|- print variables of model " + m.printRegion() + " -|-");
+        logger.debug("print variables of model " + m.printRegion());
         HashMap<String, IntVar> variablesMap = new HashMap<>();
 
         for (IntVar var : m.getModel().retrieveIntVars(true)) {
             variablesMap.put(var.getName(), var);
         }
-        
+
         for (Entry<String, IntVar> entry : variablesMap.entrySet()) {
             IntVar var = entry.getValue();
             String varDetails = String.format("  Variable Key: %s, Name: %s, Domain: [%d, %d], Current Value: %s",
                     entry.getKey(), var.getName(), var.getLB(), var.getUB(),
                     var.isInstantiated() ? String.valueOf(var.getValue()) : "Not instantiated");
-            logger.debug(varDetails);
+            logger.info(varDetails);
         }
-        logger.debug("-|- finished variables of model " + m.printRegion() + " -|-");
+        logger.debug("finished variables of model " + m.printRegion());
     }
 
     private static void printAllConstraints(BaseModel m) {
-        logger.debug("-|- print constraints of model " + m.printRegion() + " -|-");
+        logger.debug("print constraints of model " + m.printRegion());
         int cnt = 0;
         for (Constraint c : m.getModel().getCstrs()) {
-            logger.debug("  Constraint " + cnt + ": " + c.toString());
+            logger.info("  Constraint " + cnt + ": " + c.toString());
             cnt++;
         }
-        logger.debug("-|- finished constraints of model " + m.printRegion() + " -|-");
+        logger.debug("finished constraints of model " + m.printRegion());
     }
 
     /**
@@ -107,26 +114,29 @@ public class ModelMerger {
      * @param regionId      The integer ID of the region for which constraints
      *                      should apply.
      * @return A new Choco model with contextualized constraints.
-     * @throws ContradictionException 
+     * @throws ContradictionException
      */
     public static void contextualizeConstraints(BaseModel model, String variableName, Region region) {
-        //printAllVariables(newModel);
-        //printAllConstraints(newModel);
-        logger.debug("Start Contextualize of model " + model.printRegion() + " with number of constraints: " + model.getModel().getNbCstrs());
+        // printAllVariables(newModel);
+        // printAllConstraints(newModel);
+        logger.debug("Start Contextualize of model " + model.printRegion() + " with number of constraints: "
+                + model.getModel().getNbCstrs());
         for (Constraint c : model.getModel().getCstrs()) {
             if (c.getName().contains("ARITHM")) {
                 if (c.getPropagator(0) instanceof org.chocosolver.solver.constraints.binary.PropGreaterOrEqualX_Y) {
-                } 
-                
+                }
+
                 model.getModel().unpost(c);
                 Constraint cNew = Constraint.merge("ARITHM", c);
-                model.getModel().ifThen(model.getModel().arithm(getVariablesAsMap(model.getModel()).get(variableName), "=", region.ordinal()), cNew);
-            } 
+                model.getModel().ifThen(model.getModel().arithm(getVariablesAsMap(model.getModel()).get(variableName),
+                        "=", region.ordinal()), cNew);
+            }
         }
 
-        logger.debug("Finished Contextualize of model " + model.printRegion() + " with number of constraints: " + model.getModel().getNbCstrs());
-        //printAllVariables(newModel);
-        //printAllConstraints(newModel);
+        logger.debug("Finished Contextualize of model " + model.printRegion() + " with number of constraints: "
+                + model.getModel().getNbCstrs());
+        // printAllVariables(newModel);
+        // printAllConstraints(newModel);
     }
 
     private static HashMap<String, IntVar> getVariablesAsMap(Model m) {
