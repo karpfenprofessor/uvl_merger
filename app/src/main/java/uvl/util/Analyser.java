6727da3e18f639d9554b71a9c1b4a0ec944dcefa
karpfenprofessor <manuel.moertl@outlook.com>
2025-02-26 02:01:40 +0100

    removed unused logging

--- app/src/main/java/uvl/util/Analyser.java
+++ app/src/main/java/uvl/util/Analyser.java
@@ -1,14 +1,9 @@
 package uvl.util;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import uvl.model.base.BaseModel;
 import uvl.model.recreate.RecreationModel;
 
 public class Analyser {
-
-    private static final Logger logger = LogManager.getLogger(Analyser.class);
 
     public static long returnNumberOfSolutions(RecreationModel model) {
         BaseModel chocoTestModel = ChocoTranslator.convertToChocoModel(model);
