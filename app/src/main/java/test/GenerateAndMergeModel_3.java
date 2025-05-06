fc0884a9e5453a1dba817cd74de5a7995ca515b5
karpfenprofessor <manuel.moertl@outlook.com>
2025-05-06 18:37:22 +0200

    added another testcase

---
+++ app/src/main/java/test/GenerateAndMergeModel_3.java
@@ -0,0 +1,39 @@
+package test;
+
+
+import model.base.Region;
+import model.recreate.RecreationModel;
+import statistics.SolveStatistics;
+import util.Analyser;
+import util.Generator;
+import util.Merger;
+
+public class GenerateAndMergeModel_3 {
+
+    public static void main(String[] args) throws Exception {
+
+        RecreationModel mergedA = new RecreationModel(Region.A);
+        Generator.createFeatureTree(mergedA, 50);
+        Generator.createCrossTreeConstraints(mergedA, 50, 111111);
+
+        Generator.clearLastRunConstraints();
+
+        RecreationModel mergedB = new RecreationModel(Region.B);
+        Generator.createFeatureTree(mergedB, 50);
+        Generator.createCrossTreeConstraints(mergedB, 50, 111111);
+
+        Generator.clearLastRunConstraints();
+
+        //Generator.createCrossTreeConstraints(mergedA, 50, 123456);
+        //Generator.createCrossTreeConstraints(mergedB, 50, 654321);
+        
+        //List<MergeStatistics> mergeStatisticsList = Analyser.createMergeStatistics(mergedA, mergedB);
+
+        RecreationModel merged = Merger.fullMerge(mergedA, mergedB);
+        SolveStatistics solveStatistics = Analyser.createSolveStatistics(merged);
+        //System.out.println(mergeStatisticsList.toString());
+        System.out.println(solveStatistics.toString());    
+        System.out.println(Merger.getMergeStatistics().toString());
+        System.out.println(Analyser.returnNumberOfSolutions(merged));
+    }
+}
