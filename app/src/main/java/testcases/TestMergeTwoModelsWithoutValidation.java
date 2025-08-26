711bb004cad4515fa54d8e82f94f806e87b0dd83
karpfenprofessor <manuel.moertl@outlook.com>
2025-08-26 18:22:48 +0200

    results finance test run

--- app/src/main/java/testcases/TestMergeTwoModelsWithoutValidation.java
+++ app/src/main/java/testcases/TestMergeTwoModelsWithoutValidation.java
@@ -5,6 +5,8 @@ import org.apache.logging.log4j.Logger;
 
 import util.Merger;
 import util.Merger.MergeResult;
+import util.analyse.Analyser;
+import util.analyse.impl.RecreationAnalyser;
 import util.UVLParser;
 import util.Validator;
 import model.choco.Region;
@@ -15,24 +17,51 @@ public class TestMergeTwoModelsWithoutValidation {
     protected final static Logger logger = LogManager.getLogger(TestMergeTwoModelsWithoutValidation.class);
 
     public static void main(String[] args) throws Exception {
-        String modelSmartwatchAString = "uvl/smartwatch/miband1s.uvl";
-        String modelSmartwatchBString = "uvl/smartwatch/miband2.uvl";
+        String modelSmartwatchAString = "uvl/smartwatch/miband7.uvl";
+        String modelSmartwatchBString = "uvl/smartwatch/miband8.uvl";
 
-        String modelBusyboxAString = "uvl/busybox/busybox_2.uvl";
-        String modelBusyboxBString = "uvl/busybox/busybox_4.uvl";
+        String modelBusyboxAString = "uvl/busybox/busybox_1.uvl";
+        String modelBusyboxBString = "uvl/busybox/busybox_2.uvl";
+        String modelBusyboxCString = "uvl/busybox/busybox_3.uvl";
+        String modelBusyboxDString = "uvl/busybox/busybox_4.uvl";
+        String modelBusyboxEString = "uvl/busybox/busybox_5.uvl";
 
-        String modelFinanceAString = "uvl/finance/finance_2.uvl";
-        String modelFinanceBString = "uvl/finance/finance_3.uvl";
+        String modelFinanceAString = "uvl/finance/finance_1.uvl";
+        String modelFinanceBString = "uvl/finance/finance_2.uvl";
+        String modelFinanceCString = "uvl/finance/finance_3.uvl";
+        String modelFinanceDString = "uvl/finance/finance_4.uvl";
+        String modelFinanceEString = "uvl/finance/finance_5.uvl";
+        String modelFinanceFString = "uvl/finance/finance_6.uvl";
+        String modelFinanceGString = "uvl/finance/finance_7.uvl";
+        String modelFinanceHString = "uvl/finance/finance_8.uvl";
+        String modelFinanceIString = "uvl/finance/finance_9.uvl";
+        String modelFinanceJString = "uvl/finance/finance_10.uvl";
 
         String modelPaperAString = "uvl/paper_test_models/us.uvl";
         String modelPaperBString = "uvl/paper_test_models/ger.uvl";
 
-        RecreationModel originalA = UVLParser.parseUVLFile(modelBusyboxAString, Region.A);
-        RecreationModel originalB = UVLParser.parseUVLFile(modelBusyboxBString, Region.B);
+        String modelCdlAString = "uvl/cdl/vrc4373.uvl";
+        String modelCdlBString = "uvl/cdl/vrc4375.uvl";
+
+        String modelAutomotiveAString = "uvl/automotive/automotive02_01.uvl";
+        String modelAutomotiveBString = "uvl/automotive/automotive02_02.uvl";
+        String modelAutomotiveCString = "uvl/automotive/automotive02_03.uvl";
+        String modelAutomotiveDString = "uvl/automotive/automotive02_04.uvl";
+
+        RecreationModel originalA = UVLParser.parseUVLFile(modelFinanceBString, Region.A);
+        RecreationModel originalB = UVLParser.parseUVLFile(modelFinanceJString, Region.B);
+
+        RecreationAnalyser.analyseSharedFeatures(originalA, originalB);
+
+        System.out.println("originalA: " + Analyser.isConsistent(originalA));
+        System.out.println("originalB: " + Analyser.isConsistent(originalB));
 
         MergeResult mergeResult = Merger.fullMerge(originalA, originalB);
+        System.out.println("originalA: " + Analyser.isConsistent(originalA));
+        System.out.println("originalB: " + Analyser.isConsistent(originalB));
+        System.out.println("mergedModel: " + Analyser.isConsistent(mergeResult.mergedModel()));
 
-        Validator.validateMerge(mergeResult.mergedModel(), originalA, originalB);
+        // Validator.validateMerge(mergeResult.mergedModel(), originalA, originalB);
         mergeResult.mergedStatistics().printStatistics();
     }
 }
