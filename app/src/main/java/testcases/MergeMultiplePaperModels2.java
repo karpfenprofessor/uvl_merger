70d7345f1f814bbf607edc3b3d3ff527d35fe174
karpfenprofessor <manuel.moertl@outlook.com>
2025-11-18 18:15:48 +0100

    finalized multi region merge testcase for the paper

---
+++ app/src/main/java/testcases/MergeMultiplePaperModels2.java
@@ -0,0 +1,60 @@
+package testcases;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import model.choco.Region;
+import model.recreate.RecreationModel;
+import util.UVLParser;
+import util.Validator;
+import util.Merger;
+import util.Merger.MergeResult;
+import util.analyse.Analyser;
+import util.analyse.statistics.MergeStatistics;
+
+public class MergeMultiplePaperModels2 {
+    private static final Logger logger = LogManager.getLogger(MergeMultiplePaperModels2.class);
+
+    public static void main(String[] args) {
+        try {
+            RecreationModel modelUs = UVLParser.parseUVLFile("uvl/paper_test_models/union_multiple/us.uvl", Region.A);
+            RecreationModel modelGer = UVLParser.parseUVLFile("uvl/paper_test_models/union_multiple/ger.uvl", Region.B);
+            RecreationModel modelOzeania = UVLParser.parseUVLFile("uvl/paper_test_models/union_multiple/ozeania.uvl", Region.C);
+
+            /*logger.info("solutions model contextualized us: {}", Analyser.returnNumberOfSolutions(modelUs));
+            logger.info("solutions model contextualized ger: {}", Analyser.returnNumberOfSolutions(modelGer));
+            logger.info("solutions model contextualized ozeania: {}", Analyser.returnNumberOfSolutions(modelOzeania));
+
+            modelUs.contextualizeAllConstraints();
+            modelGer.contextualizeAllConstraints();
+            modelOzeania.contextualizeAllConstraints();
+
+            MergeStatistics mergeStatistics = new MergeStatistics();
+            RecreationModel unionMultipleModels = Merger.union(mergeStatistics, modelUs, modelGer, modelOzeania);
+
+            logger.info("solutions model contextualized us: {}", Analyser.returnNumberOfSolutions(modelUs));
+            logger.info("solutions model contextualized ger: {}", Analyser.returnNumberOfSolutions(modelGer));
+            logger.info("solutions model contextualized ozeania: {}", Analyser.returnNumberOfSolutions(modelOzeania));
+            logger.info("solutions union multiple model: {}", Analyser.returnNumberOfSolutions(unionMultipleModels));
+
+            RecreationModel mergedModel = Merger.inconsistencyCheck(mergeStatistics,unionMultipleModels);
+            logger.info("solutions after inconsistency check model: {}", Analyser.returnNumberOfSolutions(mergedModel));
+
+            mergedModel = Merger.cleanup(mergeStatistics, mergedModel);
+            logger.info("solutions after cleanup model: {}", Analyser.returnNumberOfSolutions(mergedModel));*/
+
+
+            MergeResult mergedModelOneStep = Merger.fullMerge(modelUs, modelGer, modelOzeania);
+            Validator.validateMerge(mergedModelOneStep.mergedModel(), modelUs, modelGer, modelOzeania);
+            mergedModelOneStep.mergedStatistics().printStatistics();
+
+            logger.info("solutions model us: {}", Analyser.returnNumberOfSolutions(modelUs));
+            logger.info("solutions model ger: {}", Analyser.returnNumberOfSolutions(modelGer));
+            logger.info("solutions model ozeania: {}", Analyser.returnNumberOfSolutions(modelOzeania));
+            logger.info("solutions merged model: {}", Analyser.returnNumberOfSolutions(mergedModelOneStep.mergedModel()));
+            Analyser.printConstraints(mergedModelOneStep.mergedModel());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
